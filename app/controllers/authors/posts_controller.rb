b9fbe5876d867a8a563ca291963dcc88c6f2f618
Jakub Skoczylas <jakub.skoczylas@gmail.com>
2018-01-01 20:51:24 +0100

    błąd w routes - nadal

--- app/controllers/authors/posts_controller.rb
+++ app/controllers/authors/posts_controller.rb
@@ -25,44 +25,44 @@ module Authors
     # POST /posts
     # POST /posts.json
     def create
-      @post = Post.new(post_params)
+        @post = Post.new(post_params)
 
-      respond_to do |format|
-        if @post.save
-          format.html { redirect_to authors_posts_path(@post), notice: 'Post was successfully created.' }
-          format.json { render :show, status: :created, location: @post }
-        else
-          format.html { render :new }
-          format.json { render json: @post.errors, status: :unprocessable_entity }
+        respond_to do |format|
+          if @post.save
+            format.html { redirect_to authors_post_path(@post), notice: 'Post was successfully created.' }
+            format.json { render :show, status: :created, location: @post }
+          else
+            format.html { render :new }
+            format.json { render json: @post.errors, status: :unprocessable_entity }
+          end
         end
       end
-    end
 
-    # PATCH/PUT /posts/1
-    # PATCH/PUT /posts/1.json
-    def update
-      respond_to do |format|
-        if @post.update(post_params)
-          format.html { redirect_to authors_posts_path(@post), notice: 'Post was successfully updated.' }
-          format.json { render :show, status: :ok, location: @post }
-        else
-          format.html { render :edit }
-          format.json { render json: @post.errors, status: :unprocessable_entity }
+      # PATCH/PUT /posts/1
+      # PATCH/PUT /posts/1.json
+      def update
+        respond_to do |format|
+          if @post.update(post_params)
+            format.html { redirect_to authors_post_path(@post), notice: 'Post was successfully updated.' }
+            format.json { render :show, status: :ok, location: @post }
+          else
+            format.html { render :edit }
+            format.json { render json: @post.errors, status: :unprocessable_entity }
+          end
         end
       end
-    end
 
-    # DELETE /posts/1
-    # DELETE /posts/1.json
-    def destroy
-      @post.destroy
-      respond_to do |format|
-        format.html { redirect_to authors_posts_url, notice: 'Post was successfully destroyed.' }
-        format.json { head :no_content }
+      # DELETE /posts/1
+      # DELETE /posts/1.json
+      def destroy
+        @post.destroy
+        respond_to do |format|
+          format.html { redirect_to authors_posts_url, notice: 'Post was successfully destroyed.' }
+          format.json { head :no_content }
+        end
       end
-    end
 
-    private
+      private
       # Use callbacks to share common setup or constraints between actions.
       def set_post
         @post = Post.friendly.find(params[:id])
